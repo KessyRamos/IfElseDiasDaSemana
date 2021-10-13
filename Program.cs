68fa4f9edc35184558b1502062316b69b9df757d
KessyRamos <90793682+KessyRamos@users.noreply.github.com>
2021-10-13 10:59:03 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IfElseDiasDaSemana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTimeOffset.Now.Date);
+
+            var mes = DateTime.Now.DayOfWeek;
+
+            if (mes == DayOfWeek.Sunday)
+            {
+                Console.WriteLine($"Today is {DayOfWeek.Sunday}");
+            }
+            else if (mes == DayOfWeek.Monday)
+            {
+                Console.WriteLine($"Today is {DayOfWeek.Monday}");
+            }
+            else if (mes == DayOfWeek.Tuesday)
+            {
+                Console.WriteLine($"Today is {DayOfWeek.Tuesday}");
+            }
+            else if (mes == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine($"Today is {DayOfWeek.Wednesday}");
+            }
+            else if (mes == DayOfWeek.Thursday)
+            {
+                Console.WriteLine($"Today is {DayOfWeek.Thursday}");
+            }
+            else if (mes == DayOfWeek.Friday)
+            {
+                Console.WriteLine($"Today is {DayOfWeek.Friday}");
+            }
+            else if (mes == DayOfWeek.Saturday)
+            {
+                Console.WriteLine($"Today is {DayOfWeek.Saturday}");
+            }
+        }
+    }
+}
